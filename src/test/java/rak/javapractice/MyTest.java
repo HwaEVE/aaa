fcb6d34c697c82bdf11392727a1872fc48f08f35
TJOEUNIS <HwaEVE@gmail.com>
2024-11-15 18:31:53 +0900

    연습

---
+++ src/test/java/rak/javapractice/MyTest.java
@@ -0,0 +1,24 @@
+package rak.javapractice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MyTest {
+
+    @Test
+    void maxTest(){
+        assertThat(isEmpty("")).isTrue();
+        assertThat(isEmpty("a")).isFalse();
+        assertThat(isEmpty("    ")).isFalse();
+    }
+boolean isEmpty (String a){
+
+
+int dsde = 1
+
+        return true;
+}
+
+
+}

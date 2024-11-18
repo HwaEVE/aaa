c698f53fd14f2a121feb876cdb3cacc4bb11bd80
TJOEUNIS <HwaEVE@gmail.com>
2024-11-18 18:26:03 +0900

    오브잭트

--- src/test/java/rak/javapractice/MyTest.java
+++ src/test/java/rak/javapractice/MyTest.java
@@ -8,17 +8,22 @@ public class MyTest {
 
     @Test
     void maxTest(){
-        assertThat(isEmpty("")).isTrue();
-        assertThat(isEmpty("a")).isFalse();
-        assertThat(isEmpty("    ")).isFalse();
+//        String 대문자 = "GO to Hell";
+//        System.out.println(대문자.startsWith("X"));
+
+//        String a = "avd";
+//        System.out.println(a.repeat(132));
+//
+//        for (int i = 1; i < 133; i++) {
+//            System.out.println(a);
+//        }
+//
+//        String 택스트1 = "나야";
+//        String 택스트2 =" ";
+//        String 택스트3 = "들기름";
+//
+//        String resust = 택스트1.concat(택스트2).concat(택스트3);
+//        System.out.println(resust);
     }
-boolean isEmpty (String a){
-
-
-int dsde = 1
-
-        return true;
-}
-
 
 }

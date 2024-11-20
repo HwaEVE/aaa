7e0873aa6a38df6ad588e5182b6b49c026df0fdc
TJOEUNIS <HwaEVE@gmail.com>
2024-11-20 18:19:38 +0900

    오브잭트

---
+++ src/test/java/rak/javapractice/DataStructure.java
@@ -0,0 +1,25 @@
+package rak.javapractice;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class DataStructure {
+
+
+
+    @Test
+    void 광역시목록() {
+        ArrayList<String> 광역시목록 = new ArrayList<>();
+
+        광역시목록.add("대전");
+        광역시목록.add("일산");
+        광역시목록.add("광주");
+        광역시목록.add("울산");
+
+        광역시목록.size();
+    }
+
+
+
+}
--- src/test/java/rak/javapractice/MyTest.java
+++ src/test/java/rak/javapractice/MyTest.java
@@ -2,28 +2,40 @@ package rak.javapractice;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyTest {
 
     @Test
-    void maxTest(){
-//        String 대문자 = "GO to Hell";
-//        System.out.println(대문자.startsWith("X"));
+    void maxTest() {
+        System.out.println(calculateArea(3.1));
+        System.out.println(calculateArea(3.3, 2.1));
+        System.out.println(calculateArea(25.3));
+    }
 
-//        String a = "avd";
-//        System.out.println(a.repeat(132));
-//
-//        for (int i = 1; i < 133; i++) {
-//            System.out.println(a);
-//        }
-//
-//        String 택스트1 = "나야";
-//        String 택스트2 =" ";
-//        String 택스트3 = "들기름";
-//
-//        String resust = 택스트1.concat(택스트2).concat(택스트3);
-//        System.out.println(resust);
+    double v = 0.0;
+
+    double calculateArea(double side) {
+        // TODO: 정사각형 면적 계산 로직을 구현하세요.
+        v = side * side;
+
+        return v;
+    }
+
+    double calculateArea(double length, double width) {
+        // TODO: 직사각형 면적 계산 로직을 구현하세요.
+
+        v = length * width;
+
+        return v;
+    }
+
+    double calculateArea(double radius, boolean isCircle) {
+        // TODO: 원의 면적 계산 로직을 구현하세요. isCircle 매개변수는 오버로딩을 위해 사용됩니다.
+
+        v = (radius * radius) * Math.PI;
+
+        return v;
     }
 
 }
